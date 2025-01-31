b127fd9c6bd308dfa06484c9908511d88b0debc9
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-01-31 12:10:57 -0500

    Update MainContainer.vue

--- app/src/components/MainContainer.vue
+++ app/src/components/MainContainer.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="flex items-center justify-center h-screen flex-1 bg-gray-600 p-4">
     <form @submit.prevent="">
-      <input class="bg-white border border-gray-300 rounded-lg p-1" type="password" />
-      <button class="" type="submit">Submit</button>
+      <input++
+        class="bg-white border border-gray-300 rounded-lg p-1"
+        type="password"
+        v-model="test.sigma"
+      />
+      <button
+        class="bg-blue-500 border border-blue-500 hover:border-blue-600 hover:bg-blue-600 hover:cursor-pointer rounded-lg p-1 m-1"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
+    <h1>{{ test }}</h1>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+const test = reactive({ sigma: '' })
+</script>
