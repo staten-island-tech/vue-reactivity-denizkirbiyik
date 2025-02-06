3986969651517cfb88ba62abbec2d6d96445d34c
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-02-06 12:30:38 -0500

    Update SideCard.vue

--- app/src/components/SideCard.vue
+++ app/src/components/SideCard.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="w-[90%] h-[200px] rounded-lg bg-gray-600 text-white p-5">
-    <h1>••••</h1>
+    <input
+      value="skibidi"
+      tabindex="-1"
+      class="!outline-none caret-transparent cursor-default"
+      v-bind:type="b ? 'password' : 'text'"
+      readonly="readonly"
+    />
+    <button
+      @click="cl"
+      class="bg-blue-500 border border-blue-500 hover:border-blue-600 hover:bg-blue-600 hover:cursor-pointer rounded-lg p-1 m-1"
+    >
+      skib
+    </button>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      b: true,
+    }
+  },
+  methods: {
+    cl() {
+      this.b = !this.b
+      console.log(this.b)
+    },
+  },
+}
+</script>
